443c696141da831d26383957954e65c6c85cda50
Azamat-Sult <asultangareev@gmail.com>
2022-03-31 16:23:28 +0300

    Dockerfiles added

--- deposit-service/src/main/java/ru/job4j/deposit/DepositApplication.java
+++ deposit-service/src/main/java/ru/job4j/deposit/DepositApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class DepositService {
+public class DepositApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DepositService.class, args);
+        SpringApplication.run(DepositApplication.class, args);
     }
 
 }
